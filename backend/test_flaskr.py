400d5e92f990c5e31998510a6168a0415d1319db
zaid <zaidalshreef@gmail.com>
2021-08-24 21:22:45 +0300

    dec

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://postgres:12345@{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}@{}/{}".format(os.getenv('server'),'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
         
         self.new_question = {
